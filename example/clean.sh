f25f026053a9e074449697d0098d4310b965d6e1
Whit Schonbein <71whit@gmail.com>
2020-11-19 08:26:43 -0700

    initial commit

---
+++ example/clean.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# for cleaning up after using e.g. texshop on mac to compile
+
+rm -f *.aux
+rm -f *.log
+rm -f *.synctex.gz
+rm -f *.toc
+rm -f *.pdf
+rm -f *.bbl
+rm -f *.bcf
+rm -f *.blg
+rm -f *.lof
+rm -f *.lot
+rm -f *.run.xml
+rm -f .DS_Store
+rm -f *.apdx
+
